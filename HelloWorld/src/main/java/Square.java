f017edb52e6f17efd7f06d084710ba2881a97ae3
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-30 18:39:54 +0400

    Square

---
+++ HelloWorld/src/main/java/Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    public static void main(String[] args) {
+        float side = 25;
+        System.out.println("Area of the Square: "
+                + side * side);
+    }
+}
