949c2b8329d19a6589efcc9b8ae5d7d88efdccb8
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-30 19:13:18 +0400

    Hello World

---
+++ HelloWorld/src/main/java/trianqle.java
@@ -0,0 +1,7 @@
+public class trianqle {
+    public static void main(String[] args) {
+        float side = 35;
+        System.out.println("Area of the Trianqle: "
+                + side * side);
+    }
+}
