b67475a7be71a1a9c144abf12de9a9c81a554f3a
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-30 19:04:28 +0400

    Hello World

---
+++ HelloWorld/src/main/java/Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        float radius = 10;
+        System.out.println("Area of the Circle: "
+                + radius * radius);
+    }
+}
