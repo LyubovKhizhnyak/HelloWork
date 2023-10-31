ed5fbb65be53b2b1aa8bb5ee864ebfa30d69a785
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-31 17:55:24 +0400

    dz corrections

--- HelloWorld/src/main/java/Circle.java
+++ HelloWorld/src/main/java/Circle.java
@@ -1,7 +1,6 @@
 public class Circle {
     public static void main(String[] args) {
         float radius = 10;
-        System.out.println("Area of the Circle: "
-                + radius * radius);
+        System.out.println("Area of the Circle: " + radius * radius);
     }
 }
