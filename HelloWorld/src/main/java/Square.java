9b84927982ac522d2c6a396da29308706d5af52d
LyubovKhizhnyak <lubakruk@ya.ru>
2023-10-31 17:59:56 +0400

    dz corrections

--- HelloWorld/src/main/java/Square.java
+++ HelloWorld/src/main/java/Square.java
@@ -1,7 +1,6 @@
 public class Square {
     public static void main(String[] args) {
         float side = 25;
-        System.out.println("Area of the Square: "
-                + side * side);
+        System.out.println("Area of the Square: " + side * side);
     }
 }
